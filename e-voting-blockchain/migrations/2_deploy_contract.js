7ca0d106d7dc51d4f3a6b84ecc0002ca53acd445
Shruti1473 <birarishruti14@gmail.com>
2025-05-16 11:37:33 +0530

    Delete e-voting-blockchain directory

--- e-voting-blockchain/migrations/2_deploy_contract.js
+++
@@ -1,6 +0,0 @@
-const Voting = artifacts.require("Voting");
-
-module.exports = function (deployer) {
-  const candidateNames = ["Narendra Modi", "Rahul Gandhi", "Mamta Banerjee"];
-  deployer.deploy(Voting, candidateNames);
-};
